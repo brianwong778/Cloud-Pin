d7d050ade725fe3e82f7318e9748bb0a4cbb155c
Brian Wong <brianwong@Brians-Air.ht.home>
2026-08-30 12:49:59 -0500

    formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-terraform{
-    backend "s3" {
-        bucket = "cloud-pin-terraform-state-bucket-1"
-        key    = "static-site/terraform.tfstate"
-        region = "us-east-1"
-    }
+terraform {
+  backend "s3" {
+    bucket = "cloud-pin-terraform-state-bucket-1"
+    key    = "static-site/terraform.tfstate"
+    region = "us-east-1"
+  }
 }

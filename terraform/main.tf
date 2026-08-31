0a48acc2106b17f24348965cd92e0f863fb64492
Brian Wong <brianwong@Brians-Air.ht.home>
2026-08-30 23:42:41 -0500

    correct bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cloud-pin-terraform-state-bucket-1"
+    bucket = "cloud-pin-terraform-state-bucket-1-812751451795-us-east-1-an"
     key    = "static-site/terraform.tfstate"
     region = "us-east-1"
   }

9a551b2af34b0d9844dde2d7a846703527eb3b59
Brian Wong <brianwong@Brians-Air.ht.home>
2026-08-30 21:34:58 -0500

    added ACM and route53 attached to my new domain

---
+++ terraform/acm.tf
@@ -0,0 +1,57 @@
+# -----------------------------
+# ACM Certificate (us-east-1)
+# -----------------------------
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+resource "aws_acm_certificate" "brianprojects" {
+  provider          = aws.us_east_1
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    var.domain_name
+  ]
+
+  tags = {
+    Project = "cloud-pin-1"
+  }
+}
+
+# -----------------------------
+# DNS Validation Records
+# -----------------------------
+resource "aws_route53_record" "brianprojects_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.brianprojects.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+
+  tags = {
+    Project = "cloud-pin-1"
+  }
+}
+
+# -----------------------------
+# Certificate Validation
+# -----------------------------
+resource "aws_acm_certificate_validation" "brianprojects" {
+  provider        = aws.us_east_1
+  certificate_arn = aws_acm_certificate.brianprojects.arn
+  validation_record_fqdns = [
+    for record in aws_route53_record.brianprojects_validation :
+    record.fqdn
+  ]
+}
---
+++ terraform/route53.tf
@@ -0,0 +1,26 @@
+# -----------------------------
+# Hosted Zone Lookup
+# -----------------------------
+data "aws_route53_zone" "primary" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+# -----------------------------
+# Subdomain -> CloudFront Alias
+# -----------------------------
+resource "aws_route53_record" "cloudpin_alias" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "cloudpin.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.site.domain_name
+    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  tags = {
+    Project = "cloud-pin-1"
+  }
+}

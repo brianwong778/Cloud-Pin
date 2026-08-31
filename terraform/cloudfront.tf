4745d3b578493b9825d3f8c05afb0f0163986288
Brian Wong <brianwong@Brians-Air.ht.home>
2026-08-31 01:17:21 -0500

    added alias, fixed

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,6 +1,7 @@
 resource "aws_cloudfront_distribution" "site" {
   enabled             = true
   default_root_object = "finalBoard.html" # First page to serve
+  aliases             = ["cloudpin.${var.domain_name}"]
 
   origin {
     domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
@@ -15,18 +16,13 @@ resource "aws_cloudfront_distribution" "site" {
     allowed_methods = ["GET", "HEAD"]
     cached_methods  = ["GET", "HEAD"]
 
-    compress        = true
-    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    compress = true
 
+    cache_policy_id            = "658327ea-f89d-4fab-a63d-7e88639e58f6"
     response_headers_policy_id = "67f7725c-6f97-4210-82d7-5512b31e9d03"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+    origin_request_policy_id   = "88a5eaf4-2fd4-4709-b370-b4c650ea3fcf"
   }
+
 
   restrictions {
     geo_restriction {
